9d6d605fcb905dd4aada9fecd4857f87ef0ca562
Octavio Galindo <octavio@euroglas.net>
2021-06-03 12:26:36 -0500

    Cambio de PHP en Pruebas
    Por alguna razón los PHP nuevos desconocieron a MySQL, usando la misma versión (5.6) del servidor.

--- iniciaServidorDePruebas.bat
+++ iniciaServidorDePruebas.bat
@@ -1,2 +1,5 @@
 REM set API_NAME=desarrollolocal
-php.exe -c .\php.ini -S 0.0.0.0:12321  index.php
+REM php.exe -c .\php.ini -S 0.0.0.0:12321  index.php
+REM php.exe -S 0.0.0.0:12321  index.php
+REM C:\php-7.4\php.exe -c .\php.ini -S 0.0.0.0:12321  index.php
+C:\php-5.6\php.exe -c .\php.ini -S 0.0.0.0:12321  index.php 
